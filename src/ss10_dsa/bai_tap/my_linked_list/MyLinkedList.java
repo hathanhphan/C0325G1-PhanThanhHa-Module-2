57422621b5d78976a3b910b3809a8b43324e6936
hathanhphan <hathanh.0113@gmail.com>
2025-05-15 16:01:23 +0700

    update

--- src/ss10_dsa/bai_tap/my_linked_list/MyLinkedList.java
+++ src/ss10_dsa/bai_tap/my_linked_list/MyLinkedList.java
@@ -1,4 +1,215 @@
 package ss10_dsa.bai_tap.my_linked_list;
 
-public class MyLinkedList {
+public class MyLinkedList<E> {
+    private Node head;
+    private int numNodes;
+
+    public MyLinkedList() {
+    }
+
+    private class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+    }
+
+    public void add(int index, E element) {
+        if (index < 0 || index > numNodes) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + numNodes);
+        }
+
+        if (index == 0) {
+            addFirst(element);
+            return;
+        }
+
+        Node temp = head;
+        Node holder;
+        for (int i = 0; i < index - 1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(element);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public void addFirst(E e) {
+        Node temp = head;
+        head = new Node(e);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public void addLast(E e) {
+        if (head == null) {
+            addFirst(e);
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = new Node(e);
+        numNodes++;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E remove(int index) {
+        if (index < 0 || index >= numNodes) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + numNodes);
+        }
+
+        Node temp = head;
+        Object removedData;
+
+        if (index == 0) {
+            removedData = head.data;
+            head = head.next;
+        } else {
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            removedData = temp.next.data;
+            temp.next = temp.next.next;
+        }
+
+        numNodes--;
+        return (E) removedData;
+    }
+
+    public boolean remove(Object e) {
+        if (head == null) {
+            return false;
+        }
+
+        if (head.data.equals(e)) {
+            head = head.next;
+            numNodes--;
+            return true;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.next.data.equals(e)) {
+                temp.next = temp.next.next;
+                numNodes--;
+                return true;
+            }
+            temp = temp.next;
+        }
+
+        return false;
+    }
+
+    public int size() {
+        return numNodes;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E clone() {
+        try {
+            MyLinkedList<E> clone = (MyLinkedList<E>) super.clone();
+            if (head == null) {
+                return (E) clone;
+            }
+
+            clone.head = new Node(head.data);
+            Node current = head.next;
+            Node cloneCurrent = clone.head;
+
+            while (current != null) {
+                cloneCurrent.next = new Node(current.data);
+                current = current.next;
+                cloneCurrent = cloneCurrent.next;
+            }
+
+            clone.numNodes = this.numNodes;
+            return (E) clone;
+        } catch (CloneNotSupportedException e) {
+            throw new InternalError(e);
+        }
+    }
+
+    public boolean contains(E o) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data.equals(o)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public int indexOf(E o) {
+        Node temp = head;
+        int index = 0;
+
+        while (temp != null) {
+            if (temp.data.equals(o)) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+
+        return -1; // Element not found
+    }
+
+    public boolean add(E e) {
+        addLast(e);
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int i) {
+        if (i < 0 || i >= numNodes) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size: " + numNodes);
+        }
+
+        Node temp = head;
+        for (int j = 0; j < i; j++) {
+            temp = temp.next;
+        }
+
+        return (E) temp.data;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E getFirst() {
+        if (head == null) {
+            throw new IndexOutOfBoundsException("List is empty");
+        }
+
+        return (E) head.data;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E getLast() {
+        if (head == null) {
+            throw new IndexOutOfBoundsException("List is empty");
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        return (E) temp.data;
+    }
+
+    public void clear() {
+        head = null;
+        numNodes = 0;
+    }
 }
--- src/ss10_dsa/bai_tap/my_linked_list/MyLinkedListTest.java
+++ src/ss10_dsa/bai_tap/my_linked_list/MyLinkedListTest.java
@@ -1,4 +1,98 @@
 package ss10_dsa.bai_tap.my_linked_list;
 
 public class MyLinkedListTest {
+    public static void main(String[] args) {
+        // Tạo một danh sách liên kết số nguyên
+        MyLinkedList<Integer> intList = new MyLinkedList<>();
+
+        System.out.println("=== Thêm phần tử vào danh sách ===");
+
+        // Thêm phần tử vào đầu danh sách
+        intList.addFirst(10);
+        System.out.println("Sau khi thêm 10 vào đầu: size = " + intList.size());
+
+        // Thêm phần tử vào cuối danh sách
+        intList.addLast(30);
+        System.out.println("Sau khi thêm 30 vào cuối: size = " + intList.size());
+
+        // Thêm phần tử vào vị trí xác định
+        intList.add(1, 20);
+        System.out.println("Sau khi thêm 20 vào vị trí 1: size = " + intList.size());
+
+        // Thêm phần tử bằng phương thức add()
+        intList.add(40);
+        System.out.println("Sau khi thêm 40 bằng add(): size = " + intList.size());
+
+        // In ra tất cả phần tử
+        System.out.println("\n=== Hiển thị danh sách ===");
+        displayList(intList);
+
+        // Lấy phần tử theo chỉ số
+        System.out.println("\n=== Lấy phần tử theo chỉ số ===");
+        System.out.println("Phần tử ở vị trí 0: " + intList.get(0));
+        System.out.println("Phần tử ở vị trí 2: " + intList.get(2));
+        System.out.println("Phần tử đầu tiên: " + intList.getFirst());
+        System.out.println("Phần tử cuối cùng: " + intList.getLast());
+
+        // Kiểm tra sự tồn tại của phần tử
+        System.out.println("\n=== Kiểm tra phần tử trong danh sách ===");
+        System.out.println("Danh sách có chứa 20 không? " + intList.contains(20));
+        System.out.println("Danh sách có chứa 50 không? " + intList.contains(50));
+
+        // Tìm vị trí của phần tử
+        System.out.println("\n=== Tìm vị trí của phần tử ===");
+        System.out.println("Vị trí của 30 trong danh sách: " + intList.indexOf(30));
+        System.out.println("Vị trí của 50 trong danh sách: " + intList.indexOf(50)); // Trả về -1 vì không tìm thấy
+
+        // Xóa phần tử theo chỉ số
+        System.out.println("\n=== Xóa phần tử theo chỉ số ===");
+        Integer removedElement = intList.remove(1);
+        System.out.println("Phần tử đã xóa ở vị trí 1: " + removedElement);
+        System.out.println("Danh sách sau khi xóa:");
+        displayList(intList);
+
+        // Xóa phần tử theo giá trị
+        System.out.println("\n=== Xóa phần tử theo giá trị ===");
+        boolean isRemoved = intList.remove(Integer.valueOf(30));
+        System.out.println("Đã xóa phần tử 30: " + isRemoved);
+        System.out.println("Danh sách sau khi xóa:");
+        displayList(intList);
+
+        // Thử xóa phần tử không tồn tại
+        isRemoved = intList.remove(Integer.valueOf(50));
+        System.out.println("Đã xóa phần tử 50: " + isRemoved);
+
+        // Thêm nhiều phần tử khác để kiểm tra
+        System.out.println("\n=== Thêm nhiều phần tử mới ===");
+        intList.add(25);
+        intList.add(35);
+        intList.add(45);
+        intList.add(55);
+        System.out.println("Danh sách sau khi thêm nhiều phần tử:");
+        displayList(intList);
+
+        // Xóa tất cả phần tử
+        System.out.println("\n=== Xóa tất cả phần tử ===");
+        intList.clear();
+        System.out.println("Kích thước sau khi xóa tất cả: " + intList.size());
+
+        // Kiểm tra danh sách rỗng
+        try {
+            System.out.println("Phần tử đầu tiên: " + intList.getFirst());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Lỗi: " + e.getMessage());
+        }
+    }
+
+    // Phương thức hiển thị tất cả phần tử trong danh sách
+    private static <E> void displayList(MyLinkedList<E> list) {
+        System.out.print("Các phần tử trong danh sách: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+            if (i < list.size() - 1) {
+                System.out.print(" -> ");
+            }
+        }
+        System.out.println("\nKích thước danh sách: " + list.size());
+    }
 }
